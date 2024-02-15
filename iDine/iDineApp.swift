6d219f0b25f93e86c8d0e5b0772343a53a38916e
Shashwat Kashyap <shashwat.kashyap@icloud.com>
2024-02-15 23:06:54 +0530

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Shashwat Kashyap on 15/02/24.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
