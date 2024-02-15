6ba26185129007f5be7bfab6fbf0d5d77973f04c
Shashwat Kashyap <shashwat.kashyap@icloud.com>
2024-02-16 00:21:50 +0530

    Added item restrictions as well in the custom view, intreestingly we used ForEach (The Bag of Legos) to show the restrictions inside a HStack

--- iDine/ItemRow.swift
+++ iDine/ItemRow.swift
@@ -10,15 +10,46 @@ import SwiftUI
 struct ItemRow: View {
     let item: MenuItem
     
+    let colors: [String: Color] = [
+        "D": .purple,
+        "G": .black,
+        "N": .red,
+        "S": .blue,
+        "V": .green,
+    ]
+    
     var body: some View {
         // Tuple view is the return type by swift, in older version it wont show up in preview without the HStack, even appeared in tutorial
         
         // For me it worked without HStack even
         HStack {
             Image(item.thumbnailImage)
+                .clipShape(Circle())
+                .overlay {
+                    // Adds circle around clipped image with some line width to make a nice boundary for styling
+                    Circle()
+                        .stroke(.gray, lineWidth: 2)
+                }
+            
             VStack(alignment: .leading) {
                 Text(item.name)
+                    .font(.headline)
                 Text("$\(item.price)")
+                    .font(.callout)
+            }
+            
+            Spacer()
+            
+            // Use the string object itself for identifying, here we have strings.
+            // Note it might not be able to seperate b/w two "S" and "S"
+            ForEach(item.restrictions, id: \.self) { restriction in
+                Text(restriction)
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(5)
+                    .background(colors[restriction, default: .black])
+                    .clipShape(Circle())
+                    .foregroundColor(.white)
             }
         }
     }
