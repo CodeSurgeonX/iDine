0c0440c1f4ae497be6840fa4882364c23c24c9b4
Shashwat Kashyap <shashwat.kashyap@icloud.com>
2024-02-16 18:16:43 +0530

    Light weight abstractions created

--- iDine/CheckoutView.swift
+++ iDine/CheckoutView.swift
@@ -11,22 +11,61 @@ struct CheckoutView: View {
     @EnvironmentObject var order: Order
     // Simple values and only for current view, always make private apple recommends
     @State private var paymentType = "Cash"
+    @State private var addLoyaltyDetails = false
+    @State private var loyaltyNumber = ""
+    @State private var tipAmount = 15
     
+    let tipAmounts = [10,15,20,25,0]
     let paymentTypes = [
         "Cash",
         "Credit Card",
         "iDine Points"
     ]
     
-    var body: some View {
+    
+    var paymentPickerView: some View {
         VStack {
-            Section {
-                // $ is signifying the binding instead of just the var, it is like a reference has been passed to picker and whenever use interacts with picker the value is set to this variable
-                Picker("How do you want to pay", selection: $paymentType) {
-                    ForEach(paymentTypes, id:\.self) { paymentType in
-                        Text(paymentType)
-                    }
+            // $ is signifying the binding instead of just the var, it is like a reference has been passed to picker and whenever use interacts with picker the value is set to this variable
+            Picker("How do you want to pay", selection: $paymentType) {
+                ForEach(paymentTypes, id:\.self) {
+                    Text($0)
                 }
+            }
+            
+            // We are adding animation to the binding, so whenever a value changes.
+            Toggle("Add iDine Loyalty Card", isOn: $addLoyaltyDetails.animation(.linear))
+            if addLoyaltyDetails {
+                TextField("Enter your iDine id", text: $loyaltyNumber)
+                    .transition(.scale)
+            }
+        }
+    }
+    
+    var tipView: some View {
+        Picker("Percentage", selection: $tipAmount) {
+            ForEach(tipAmounts, id: \.self) { Text("\($0)") }
+        }
+        .pickerStyle(.segmented)
+    }
+    
+    var totalView: some View {
+        Color.red
+    }
+    
+    var body: some View {
+        // Form makes this look perfect compared to VSTack, might wanna read more around it on apple doc
+        Form {
+            // Used with list form and picker
+            Section {
+                paymentPickerView
+            }
+            
+            Section("Add a tip") {
+                tipView
+            }
+            
+            Section("Total") {
+                totalView
             }
         }
         .navigationTitle("Payment")
